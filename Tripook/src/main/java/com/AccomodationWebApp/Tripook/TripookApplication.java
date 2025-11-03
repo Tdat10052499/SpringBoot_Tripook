ad0a31b3bd0bb11e513e4f23cc35a2d696c413a3
Hồ Du Tuấn Đạt <tdat.100524@gmail.com>
2025-11-03 15:11:40 +0700

    Add files via upload

---
+++ Tripook/src/main/java/com/AccomodationWebApp/Tripook/TripookApplication.java
@@ -0,0 +1,13 @@
+package com.AccomodationWebApp.Tripook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripookApplication.class, args);
+	}
+
+}
---
+++ Tripook/src/test/java/com/AccomodationWebApp/Tripook/TripookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccomodationWebApp.Tripook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
